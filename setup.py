c1422ceca2d2fb17df7981534ba97de0ea5f0eaf
Gael Pasgrimaud <gael@gawel.org>
2015-02-23 19:32:37 +0100

    Back to development: 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.2'
+version = '0.3.dev0'
 
 install_requires = ['croniter']
 test_requires = ['coverage']
