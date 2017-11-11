7697713e7a0cee3712958578fed8a1ad8be64b0c
Gael Pasgrimaud <gael@gawel.org>
2017-11-11 11:45:03 +0100

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '1.1'
+version = '1.2.dev0'
 
 install_requires = ['croniter', 'tzlocal']
 test_requires = ['coverage']
