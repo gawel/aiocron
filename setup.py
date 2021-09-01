35f0ea863ea94c81255468a719489b30bf3e6120
Gael Pasgrimaud <gael@gawel.org>
2021-09-01 10:51:53 +0200

    Back to development: 1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-version = '1.7'
+version = '1.8.dev0'
 
 install_requires = ['croniter', 'tzlocal']
 test_requires = ['coverage']
